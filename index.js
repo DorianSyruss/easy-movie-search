64b3a9f9fbfdb5992a471106a738a55b54d60de9
engine <tcd-kakaroto@hotmail.com>
2017-02-22 18:15:29 +0100

    almost done

--- index.js
+++ index.js
@@ -3,10 +3,58 @@
  */
 import 'whatwg-fetch';
 
-let url = 'http://www.imdb.com/year/2017/'
-fetch(url).then(function (response) {
-    console.log(response.status);
-    //handle http request
-}), function (error) {
-    //handle network error
+const proxy = 'https://cors.now.sh/';
+const url = 'http://www.imdb.com/year/2017/'
+const fullUrl = `${proxy}${url}`;
+
+function fetchDocument(url) {
+    return fetch(url)
+        .then((response) => response.json())
+        .then(html => parseHtml(html))
 }
+
+const map = (iterable, fn) => [].map.call(iterable, fn);
+
+function parseHtml(html) {
+    let parser = new DOMParser();
+    return parser.parseFromString(html, 'text/html');
+}
+
+function findOne($context, selector) {
+    return $context.querySelector(selector) || { textContent: '' };
+}
+
+function getMovies($doc) {
+    let $row = $doc.querySelectorAll('table.results tr');
+    return map($row, parseMovie);
+}
+
+function parseMovie($row) {
+    let title = findOne($row, 'td.title > a').textContent.trim();
+    let rating = parseFloat(findOne($row, 'span.rating-rating').textContent.trim().split('/')[0]) || undefined;
+    let desc = findOne($row, 'span.outline').textContent.trim();
+    let imgUrl = findOne($row, '.image img').getAttribute('src');
+
+    return {
+        title,
+        rating,
+        desc,
+        imgUrl
+    }
+}
+
+function renderMovie($output, movie) {
+    let html = `<div class = "movie">${movie.title}</div>`;
+    $output.innerHTML += html;
+}
+
+
+function renderMovies(movies) {
+    let $output = document.querySelector('.movies');
+    movies.forEach(movie => renderMovie($output, movie));
+}
+
+fetchDocument(fullUrl)
+    .then($doc => getMovies($doc))
+    .then(movies => renderMovies(movies));
+

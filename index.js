c31d9e7080c4bc6e3fbc3b2ecb165e6f245d7648
engine <tcd-kakaroto@hotmail.com>
2017-02-23 13:36:52 +0100

    added UI, more features, code polished

--- index.js
+++ index.js
@@ -4,57 +4,73 @@
 import 'whatwg-fetch';
 
 const proxy = 'https://cors.now.sh/';
-const url = 'http://www.imdb.com/year/2017/'
+const url = 'http://www.imdb.com/year/2017/';
 const fullUrl = `${proxy}${url}`;
 
 function fetchDocument(url) {
-    return fetch(url)
-        .then((response) => response.json())
-        .then(html => parseHtml(html))
+  return fetch(url)
+    .then((response) => response.json())
+    .then(html => parseHtml(html))
 }
 
 const map = (iterable, fn) => [].map.call(iterable, fn);
 
 function parseHtml(html) {
-    let parser = new DOMParser();
-    return parser.parseFromString(html, 'text/html');
+  let parser = new DOMParser();
+  return parser.parseFromString(html, 'text/html');
 }
 
 function findOne($context, selector) {
-    return $context.querySelector(selector) || { textContent: '' };
+  return $context.querySelector(selector) || {textContent: ''};
 }
 
 function getMovies($doc) {
-    let $row = $doc.querySelectorAll('table.results tr');
-    return map($row, parseMovie);
+  let $row = $doc.querySelectorAll('table.results tr');
+  return map($row, parseMovie);
 }
 
 function parseMovie($row) {
-    let title = findOne($row, 'td.title > a').textContent.trim();
-    let rating = parseFloat(findOne($row, 'span.rating-rating').textContent.trim().split('/')[0]) || undefined;
-    let desc = findOne($row, 'span.outline').textContent.trim();
-    let imgUrl = findOne($row, '.image img').getAttribute('src');
+  let title = findOne($row, 'td.title > a').textContent.trim();
+  let titleUrl = findOne($row, 'td.title > a').getAttribute('href');
+  let rating = parseFloat(findOne($row, 'span.rating-rating').textContent.trim().split('/')[0]) || undefined;
+  let desc = findOne($row, 'span.outline').textContent.trim();
+  let imgUrl = findOne($row, '.image img').getAttribute('src');
+  let genre = findOne($row, '.title .genre').textContent.trim();
 
-    return {
-        title,
-        rating,
-        desc,
-        imgUrl
-    }
+  return {
+    title,
+    titleUrl,
+    rating,
+    desc,
+    imgUrl,
+    genre,
+  }
 }
 
 function renderMovie($output, movie) {
-    let html = `<div class = "movie">${movie.title}</div>`;
-    $output.innerHTML += html;
+  const movieUrl = `http://www.imdb.com${movie.titleUrl}`;
+  let html = `<div class = "movie">
+       <div class="title"><a href ="${movieUrl}" target="_blank">${movie.title}</a></div> 
+           <img src="${movie.imgUrl}" alt="">
+           <div class = "content-right">
+               <div class="rating">${movie.rating || 'not rated' }</div> 
+               <div class="desc">${movie.desc}</div>
+               <div class="genre">${movie.genre}</div>
+           </div>
+        </div>`;
+  $output.innerHTML += html;
 }
 
 
 function renderMovies(movies) {
-    let $output = document.querySelector('.movies');
-    movies.forEach(movie => renderMovie($output, movie));
+  let $output = document.querySelector('.movies');
+  movies.forEach(movie => renderMovie($output, movie));
 }
 
 fetchDocument(fullUrl)
-    .then($doc => getMovies($doc))
-    .then(movies => renderMovies(movies));
+  .then($doc => getMovies($doc))
+  .then(movies => {
+    console.table(movies);
+    renderMovies(movies);
+  });
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,18 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'bundle.js',
-        path: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            }
-        ]
-    }
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
 };
 

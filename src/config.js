01622dd44aba1d68cb6e50f99841b67fc3ceb9c5
Ivan Saric <tcd-kakaroto@hotmail.com>
2019-07-22 16:53:59 +0200

    Add new cors now proxy server

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // backupProxyUrl: 'http://cors-anywhere.herokuapp.com/'
-  proxyUrl: 'https://cors.now.sh/',
+  proxyUrl: 'https://cors-anywhere.herokuapp.com/',
   baseUrl: 'http://www.imdb.com/',
   maxYear: 2030,
   minYear: 1950,
